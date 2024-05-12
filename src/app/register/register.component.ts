bb47706ef740521b1c980c103f212aa83ed11345
techikram <ahdadoucheikram@gmail.com>
2024-05-12 17:36:30 +0200

    fixing values

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -9,8 +9,8 @@ export class RegisterComponent implements OnInit {
   username: string = '';
   email: string = '';
   password: string = '';
-  number: string = ''; // New field for number
-  hospitalName: string = ''; // New field for hospital name
+  number: string = '';
+  hospitalName: string = ''; 
 
   constructor() { }
 
@@ -18,13 +18,13 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit() {
-    // You can handle the registration logic here, such as sending the form data to a backend server
+   
     console.log('Username:', this.username);
     console.log('Email:', this.email);
     console.log('Password:', this.password);
     console.log('Number:', this.number);
     console.log('Hospital Name:', this.hospitalName);
-    // Reset the form after submission
+  
     this.username = '';
     this.email = '';
     this.password = '';
